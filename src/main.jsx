7d260336cb2988d1df58b8fe44778cf24c862c16
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-12-10 22:02:57 -0300

    direcionamento das páginas

---
+++ src/main.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { createBrowserRouter, RouterProvider, Route} from 'react-router-dom'
+import './index.css'
+
+// Paginas
+import Cadastro from './routes/Cadastro'
+import Home from './routes/Home'
+import Produtos from './routes/Produtos'
+import DadosClientes from './routes/DadosClientes'
+import Login from './routes/Login'
+
+const Private = ({ Item })=>{
+  const signed = false 
+
+  return signed > 0 ? <Item /> : <Login />
+}
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+       path: "/",
+       element: <Home />
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/cadastro",
+        element: <Cadastro />
+      },
+      {
+        path: "/dadosclientes",
+        element: <DadosClientes />
+      },
+      {
+        path: "*",
+        element: <Cadastro /> 
+      }
+
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
