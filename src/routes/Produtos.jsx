f01368c3c67a9dfb8954581fd82faec6a0304af8
SouzaF98 <110492173+SouzaF98@users.noreply.github.com>
2022-12-11 23:13:10 -0300

    Add files via upload

---
+++ src/routes/Produtos.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useCart } from "../components/CartContext";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./Produtos.css";
+
+function Produtos() {
+  const [produtos, setProdutos] = useState([]);
+
+  const getProdutos = async () => {
+    try {
+      const response = await axios.get(
+        "https://ironshape.onrender.com/produtos"
+      );
+      const data = response.data;
+
+      setProdutos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProdutos();
+  }, []);
+
+  const cart = useCart();
+
+  const add = product => () => {
+    cart.addToCart(product);
+  };
+
+  return (
+    <>
+      <div className="row">
+        {produtos.map(produto => (
+          <div
+            className="card green"
+            key={produto.id}
+          >
+            <img
+              className="image"
+              src={produto.imagem}
+              alt={produto.name}
+            />
+
+            <h2>{produto.name}</h2>
+            <h3>R${produto.valor}</h3>
+            <button
+              className="btnCarrinho"
+              onClick={add(produto)}
+            >
+              Adicionar ao Carrinho
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Produtos;
